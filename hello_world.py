8694f010ef36156a07e2f48963fb3623eaf03f0e
traviskennedy267 <travis.a.kennedy@gmail.com>
2023-11-28 19:37:01 -0700

    update

--- hello_world.py
+++
@@ -1,8 +0,0 @@
-name = input("What is your name? ")
-favorite_color = input("What is your favorite color? ")
-print(name + " likes " + favorite_color)
-
-print ("my name is travis")
-
-a = 7
-print(a)
